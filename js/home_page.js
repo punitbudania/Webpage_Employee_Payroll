33d44dc9f856fd47e905e892d7bec74f0a2819af
punitbudania <punitbudania88@gmail.com>
2021-01-06 19:57:05 +0530

    [punit]Added: used template literals to populate the inner html

--- js/home_page.js
+++ js/home_page.js
@@ -4,21 +4,32 @@ window.addEventListener('DOMContentLoaded', (Event) =>{
 
 const createInnerHtml = () => {
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>";
-    const innerHtml = `${headerHtml}
-    <tr>
-        <td><img src="../assets/Ellipse -2.png" alt="2" class="profile"></td>
-        <td>Nuke</td>
-        <td>Male</td>
-        <td>
-            <div class="dept-label">HR</div>
-            <div class="dept-label">Finance</div>
-        </td>
-        <td>409000</td>
-        <td>1 Nov 2020</td>
-        <td>
-            <img id="1" onclick="remove(this)" src="../assets/delete-black-18dp.svg" alt="delete">
-            <img id="1" onclick="update(this)" src="../assets/create-black-18dp.svg" alt="create">
-        </td>
-    </tr>`;
+    const innerHtml = `${headerHtml}`;
+    let empPayrollList = createEmplo();
+    for (const empPayrollData of empPayrollList)
+    {
+        innerHtml = `${innerHtml}
+        <tr>
+            <td><img src="${empPayrollData._profilePic}" alt="2" class="profile"></td>
+            <td>${empPayrollData._name}</td>
+            <td>${empPayrollData._gender}</td>
+            <td>${getDeptHtml(empPayrollData._department)}</td>
+            <td>${empPayrollData._salary}</td>
+            <td>${empPayrollData._startDate}</td>
+            <td>
+                <img name="${empPayrollData._id}" onclick="remove(this)" src="../assets/delete-black-18dp.svg" alt="delete">
+                <img id="${empPayrollData._id}" onclick="update(this)" src="../assets/create-black-18dp.svg" alt="create">
+            </td>
+        </tr>`;
+    }
     document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for (const dept of deptList)
+    {
+        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+    }
+    return deptHtml;
 }
